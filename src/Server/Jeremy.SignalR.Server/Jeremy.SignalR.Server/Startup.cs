e65a2c4221d90aa22b395ea0a6d2c7af7db30b47
jeremy <1280442096@qq.com>
2020-11-23 10:34:52 +0800

    Adding project server files

---
+++ src/Server/Jeremy.SignalR.Server/Jeremy.SignalR.Server/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin.Host.SystemWeb;
+
+[assembly: OwinStartup(typeof(Jeremy.SignalR.Server.Startup))]
+
+namespace Jeremy.SignalR.Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 有关如何配置应用程序的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkID=316888
+
+            // 配置集线器
+            app.MapSignalR();
+        }
+    }
+}
